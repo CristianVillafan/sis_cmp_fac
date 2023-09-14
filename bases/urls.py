37e1e14fb1a0f896ce27fc57c6b00a2afbe13814
CristianVillafan <villafan_cristian@outlook.com>
2023-09-14 17:03:45 -0600

    Logout agregado, con mensajes de error al login

--- bases/urls.py
+++ bases/urls.py
@@ -5,4 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'), name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='bases/login.html'), name='logout'),
 ]
