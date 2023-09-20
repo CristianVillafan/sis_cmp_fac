8f4ee27a2be6c5106c75751e4cfc1ef2985d7515
CristianVillafan <villafan_cristian@outlook.com>
2023-09-19 20:04:13 -0600

    Despues de implementar el catalogo categoria se agregaron los demas catalogos en la app inv que son: Subcategoria, Marca y Unidades de medida , con sus respectivas funciones, crear, editar, borrar, inactivate. Tambien se agregaron las plantillas html correspondientes junto con sus migraciones

--- inv/models.py
+++ inv/models.py
@@ -15,4 +15,52 @@ class Categoria(ClaseModelo):
         super(Categoria, self).save()
 
     class Meta:
-        verbose_name_plural = "categorias"
+        verbose_name_plural = "Categorias"
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length = 100,
+        help_text= 'Descripcion de la subcategoria'
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion, self.descripcion)
+    
+    def save(self):
+        self.descripcion=self.descripcion.upper()
+        super(SubCategoria, self).save()
+
+    class Meta:
+        verbose_name_plural = "Sub Categorias"
+        unique_together = ('categoria','descripcion')
+
+class Marca(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la marca',
+        unique=True
+        )
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    def save(self):
+        self.descripcion=self.descripcion.upper()
+        super(Marca, self).save()
+
+    class Meta:
+        verbose_name_plural = "Marcas"
+
+class UnidadMedida(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la unidad de medida',
+        unique=True
+        )
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    def save(self):
+        self.descripcion=self.descripcion.upper()
+        super(UnidadMedida, self).save()
+
+    class Meta:
+        verbose_name_plural = "Unidades de medida"
--- inv/urls.py
+++ inv/urls.py
@@ -1,5 +1,26 @@
 from django.urls import path
-from .views import CategoriaView
+from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel, SubCategoriaView, SubCategoriaNew, SubCategoriaEdit, SubCategoriaDel, MarcaView, MarcaNew, MarcaEdit , marca_inactive, UnidadMedidaView, UnidadMedidaNew, UnidadMedidaEdit, unidad_medida_inactive
 urlpatterns = [
-    path('categoria/', CategoriaView.as_view(), name='categoria_list'),
+    path('categorias/', CategoriaView.as_view(), name='categoria_list'),
+    path('categorias/new', CategoriaNew.as_view(), name='categoria_new'),
+    path('categorias/edit/<int:pk>', CategoriaEdit.as_view(), name='categoria_edit'),
+    path('categorias/delete/<int:pk>', CategoriaDel.as_view(), name= 'categoria_del'),
+
+    path('subcategorias/', SubCategoriaView.as_view(), name='subcategoria_list'),
+    path('subcategoria/new', SubCategoriaNew.as_view(), name='subcategoria_new'),
+    path('subcategorias/edit/<int:pk>', SubCategoriaEdit.as_view(), name='subcategoria_edit'),
+    path('subcategorias/delete/<int:pk>', SubCategoriaDel.as_view(), name= 'subcategoria_del'),
+
+    path('marcas/', MarcaView.as_view(), name='marca_list'),
+    path('marcas/new', MarcaNew.as_view(), name='marca_new'),
+    path('marcas/edit/<int:pk>', MarcaEdit.as_view(), name='marca_edit'),
+    path('marcas/inactive/<int:id>', marca_inactive , name='marca_inactive'),
+
+    path('unidades_de_medida', UnidadMedidaView.as_view(), name='unidad_medida_list'),
+    path('unidades_de_medida/new', UnidadMedidaNew.as_view(), name = 'unidad_medida_new'),
+    path('unidades_de_medida/edit/<int:pk>', UnidadMedidaEdit.as_view(), name='unidad_medida_edit'),
+    path('unidades_de_medida/inactive/<int:id>', unidad_medida_inactive , name='unidad_medida_inactive'),
+
+
+
 ]
